0192bee76082e3af9c298d06a0b9dfe43ab50518
bigkastus <bigkastus@gmail.com>
2020-10-25 21:32:21 +0300

    feat:add solution-11passed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-    let stack = [];
-  let lastBr;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == '[' || str[i] == '(' || str[i] == '<') {
-            stack.push(str[i]);
-        } else if (str[i] == ']' || str[i] == ')' || str[i] == '}' || str[i] == '>') {
-            if (stack.length) {
-              lastBr = stack[stack.length - 1];
-              if ( (lastBr == '[' && str[i] == ']') ||
-                   (lastBr == '(' && str[i] == ')') ||
-                   (lastBr == '<' && str[i] == '>') ) {
-                    stack.pop();
-              }
-            } else return false;
+    
+        let stack = []        
+        
+        for(let bracket of str) {                       
+            let bracketsIndex = bracketsConfig.indexOf(bracket)
+      
+          if (bracketsIndex === -1){
+            continue
           }
-    }
-  return (!stack.length);
+      
+          if(bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1)
+          } else {            
+            if(stack.pop() !== bracketsIndex) {                
+              return false;              
+            }
+          }
+        }   
+        return stack.length === 0                    
+
+
+
+//     let stack = [];
+//   let lastBr;
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] == '[' || str[i] == '(' || str[i] == '<') {
+//             stack.push(str[i]);
+//         } else if (str[i] == ']' || str[i] == ')' || str[i] == '}' || str[i] == '>') {
+//             if (stack.length) {
+//               lastBr = stack[stack.length - 1];
+//               if ( (lastBr == '[' && str[i] == ']') ||
+//                    (lastBr == '(' && str[i] == ')') ||
+//                    (lastBr == '<' && str[i] == '>') ) {
+//                     stack.pop();
+//               }
+//             } else return false;
+//           }
+//     }
+//   return (!stack.length);
 }
