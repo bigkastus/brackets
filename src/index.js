9d90aa55584f8f94da96813982938652c3daaeb2
bigkastus <bigkastus@gmail.com>
2020-10-25 21:26:27 +0300

    feat:add solution-17passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+  let lastBr;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == '[' || str[i] == '(' || str[i] == '<') {
+            stack.push(str[i]);
+        } else if (str[i] == ']' || str[i] == ')' || str[i] == '}' || str[i] == '>') {
+            if (stack.length) {
+              lastBr = stack[stack.length - 1];
+              if ( (lastBr == '[' && str[i] == ']') ||
+                   (lastBr == '(' && str[i] == ')') ||
+                   (lastBr == '<' && str[i] == '>') ) {
+                    stack.pop();
+              }
+            } else return false;
+          }
+    }
+  return (!stack.length);
 }
